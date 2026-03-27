3cf9717a2ac114b50f36fa8d74c07eab319e251e
camilo-villada <villajuan163@gmail.com>
2026-03-27 17:36:11 -0500

    ejercicios semana 1

---
+++ semana_1/src/model/EmpresaRecord.java
@@ -0,0 +1,5 @@
+package model;
+
+// Record: menos codigo para datos inmutables; clase tradicional: mejor cuando hay mas comportamiento.
+public record EmpresaRecord(String nombre, String nit, int anioFundacion) {
+}
